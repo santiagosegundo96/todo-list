cb70f6153de48511f4214bb30dbaccec44405aab
santi <santiago.segundo@fing.edu.uy>
2022-08-30 23:15:58 -0300

    wip change component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Container from './components/Container';
+import Form from './components/Form';
 
 function App() : React.ReactElement {
-  return (
-    <div className="App">
-      <Container />
-    </div>
-  );
+    return (
+        <Form />
+    );
 }
 
 export default App;
